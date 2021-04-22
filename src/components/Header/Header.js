8e4c6a4a03d85d31cd5955d471aff3d56e359e0f
Vyacheslav Chuletc <chuletsva@gmail.com>
2021-04-22 21:06:22 +0200

    ...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,15 @@ import { Button, Box, Typography, makeStyles } from "@material-ui/core";
 const CURRENT_USER = "Чулец Вячеслав Анатольевич";
 
 const useStyles = makeStyles((theme) => ({
-  header: {},
+  header: {
+    display: "flex",
+    justifyContent: "space-around",
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+  },
   page: {},
   icon: {
+    fill: theme.palette.primary.contrastText,
     width: 50,
     height: 50,
   },
@@ -15,19 +21,14 @@ const Header = ({ title, Icon }) => {
   const classes = useStyles();
 
   return (
-    <Box
-      display="flex"
-      justifyContent="space-around"
-      bgcolor="primary.main"
-      color="primary.contrastText"
-    >
+    <Box className={classes.header}>
       <Box>
         <Icon className={classes.icon} />
         <Typography variant="h3">{title}</Typography>
       </Box>
 
       <Box>
-        <Typography variant="caption">{CURRENT_USER}</Typography>
+        <Typography variant="h5">{CURRENT_USER}</Typography>
         <Button variant="contained" color="primary">
           Выйти
         </Button>
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+import { Box, Grid, Typography, makeStyles } from "@material-ui/core";
+
+import Header from "components/Header/Header";
+
+import { ReactComponent as StarIcon } from "assets/icons/star.svg";
+import { ReactComponent as HouseIcon } from "assets/icons/house.svg";
+import { ReactComponent as ClientsIcon } from "assets/icons/clients.svg";
+import { ReactComponent as MessagesIcon } from "assets/icons/messages.svg";
+import { ReactComponent as BroadcastIcon } from "assets/icons/broadcast.svg";
+import { ReactComponent as EmployeesIcon } from "assets/icons/employees.svg";
+import { ReactComponent as AppointmentIcon } from "assets/icons/appointment.svg";
+
+const TITLE = "Домашняя";
+
+const NAVIGATION_ITEMS = [
+  { title: "Приёмы", Icon: AppointmentIcon },
+  { title: "События", Icon: StarIcon },
+  { title: "Оповещения", Icon: BroadcastIcon },
+  { title: "Сообщения", Icon: MessagesIcon },
+  { title: "Клиенты", Icon: ClientsIcon },
+  { title: "Сотрудники", Icon: EmployeesIcon },
+];
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "2px dashed",
+  },
+
+  navigationPanel: {
+    width: "fit-content",
+  },
+
+  navigationItem: {
+    border: "5px solid",
+    borderRadius: 10,
+    borderColor: theme.palette.primary.main,
+    color: theme.palette.primary.main,
+    width: 130,
+    height: 130,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+
+  icon: {
+    fill: theme.palette.primary.main,
+  },
+}));
+
+const NavigationItem = (props) => {
+  const { title, Icon } = props;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.navigationItem}>
+      <Icon className={classes.icon} />
+      <Typography variant="h5">{title}</Typography>
+    </Box>
+  );
+};
+
+const NavigationPanel = (props) => {
+  const classes = useStyles();
+
+  const items = NAVIGATION_ITEMS.map((item) => (
+    <Grid item xs={4} key={item.title}>
+      <NavigationItem {...item} />
+    </Grid>
+  ));
+
+  return (
+    <div className={classes.navigationPanel}>
+      <Grid container spacing={1}>
+        {items}
+      </Grid>
+    </div>
+  );
+};
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Header Icon={HouseIcon} title={TITLE} />
+
+      <Box className={classes.body}>
+        <NavigationPanel />
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
